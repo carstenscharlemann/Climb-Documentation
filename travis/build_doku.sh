144a7a6e55c4350929be164e371b9f042dd33aec
RobertK66 <Robert.climb@rkos.at>
2018-07-05 18:20:38 +0200

    add arc42 template

--- travis/build_doku.sh
+++ travis/build_doku.sh
@@ -2,4 +2,7 @@ rm mdpdf/*.*
 echo Building PDFs...
 cd md
 pandoc --latex-engine=xelatex *.md --output ../mdpdf/test1.pdf
+cd ..
+#pandoc --latex-engine=xelatex md/CLI_ADD_RKS_001/*.md -f markdown_github --output mdpdf/CLI_ADD_RKS_001.pdf
+pandoc -s -r html https://github.com/carstenscharlemann/Climb-Documentation/blob/master/md/CLI_ADD_RKS_001/overview.md --latex-engine=xelatex -o mdpdf/test2.pdf
 
